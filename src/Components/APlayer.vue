74fbc4db5f43c33657219e2c627bc1a9784a4da6
= <=>
2025-01-11 17:01:43 +0100

    updated version and added music-metadata as dep

--- src/Components/APlayer.vue
+++ src/Components/APlayer.vue
@@ -640,32 +640,6 @@ console.log("store: ", store)
       fetchImage();
     });
   };
-  
-  const base64ToBlob = (base64: string, mime: string) => {
-    mime = mime || '';
-    var sliceSize = 1024;
-    var byteChars = window.atob(base64);
-    var byteArrays = [];
-  
-    for (
-      var offset = 0, len = byteChars.length;
-      offset < len;
-      offset += sliceSize
-    ) {
-      var slice = byteChars.slice(offset, offset + sliceSize);
-  
-      var byteNumbers = new Array(slice.length);
-      for (var i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-  
-      var byteArray = new Uint8Array(byteNumbers);
-  
-      byteArrays.push(byteArray);
-    }
-  
-    return new Blob(byteArrays, { type: mime });
-  };
 
   const base64ToByteArray = (base64: string): Uint8Array => {
     const sliceSize = 1024;
